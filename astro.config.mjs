d09c73b5913687fae41482858677948c948dfba7
minhvip08 <minhvip08@gmail.com>
2024-10-13 23:02:45 +0700

    chore: init

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import astroIcon, icon from 'astro-icon';
+import playformCompress from "@playform/compress";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), astroIcon({
+    include: {
+      mdi: ["*"],
+      'ri': ['*'],
+      'simple-icons': ['*'],
+    },
+  }), playformCompress({
+    CSS: false,
+    Image: false,
+    Action: {
+      Passed: async () => true,   // https://github.com/PlayForm/Compress/issues/376
+    },
+  }), icon()],
+});
